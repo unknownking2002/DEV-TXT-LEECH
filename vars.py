4636bf5a0b5c9d16d7dcdf2662801ed647fbcfcd
unknownking <133520032+unknownking2002@users.noreply.github.com>
2026-01-03 13:49:23 +0530

    Refactor environment variable handling in vars.py

--- vars.py
+++ vars.py
@@ -9,7 +9,8 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 CREDIT = os.environ.get("CREDIT", "⌯ FʀᴏɴᴛMᴀɴ | ×͜× |")
 # MongoDB Configuration
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "DevThanos")
-DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Dev-Thanos")  # Add your own atlas db
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://ravimishra200101:L6js4FKiDltyWUGB@cluster0.znjbdbv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+")  # Add your own atlas db
 MONGO_URL = DATABASE_URL  # For auth system
 
 # Owner and Admin Configuration
@@ -59,6 +60,7 @@ Please contact the admin @ItsUGBot to get access.</blockquote>""",
 
 <blockquote>Use format: {format}</blockquote>"""
 }
+
 
 
 
